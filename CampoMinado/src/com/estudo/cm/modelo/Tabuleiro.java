5149aff9a27887e522536d502c114a7774aed71d
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2020-05-10 21:10:26 -0300

    Ex. tabuleiro 01 and tabuleiro 02

--- CampoMinado/src/com/estudo/cm/modelo/Tabuleiro.java
+++ CampoMinado/src/com/estudo/cm/modelo/Tabuleiro.java
@@ -1,5 +1,65 @@
 package com.estudo.cm.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<Campo>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha,coluna));
+			}
+		}
+	}
+	
+	private void associarVizinhos() {
+		for (Campo campo1: campos) {
+			for (Campo campo2: campos) {
+				campo1.adicionarVizinho(campo2);
+			}
+		}
+	}
+	
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = campoMinado -> campoMinado.isMinado();
+		
+		do {
+			int aleatorio = (int) (Math.random() * campos.size());
+			minasArmadas = campos.stream().filter(minado).count();
+			
+			campos.get(aleatorio).minar();			
+		} while (minasArmadas < minas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
+	}
+	
 }
